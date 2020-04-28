36bc6ec856dd2e5a4073b119d8240b2ae7892d0c
Zi Yin <yinzi.aiesecjinan@gmail.com>
2020-04-28 09:42:31 +0100

    update a dash app

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -6,6 +5,8 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
 
+import dash
+from flask.helpers import get_root_path
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -35,4 +36,28 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(errors)
 
+    from flaskblog.tradings.layout import layout as layout1
+    from flaskblog.tradings.callbacks import register_callbacks as register_callbacks1
+    register_dashapp(app, 'Dashapp', 'tradings',
+                     layout1, register_callbacks1)
+
     return app
+
+
+def register_dashapp(app, title, base_pathname, layout, register_callbacks_fun):
+    # Meta tags for viewport responsiveness
+    meta_viewport = {"name": "viewport",
+                     "content": "width=device-width, initial-scale=1, shrink-to-fit=no"}
+
+    my_dashapp = dash.Dash(__name__,
+                           server=app,
+                           url_base_pathname=f'/{base_pathname}/',
+                           assets_folder=get_root_path(
+                               __name__) + f'/{base_pathname}/assets/',
+                           meta_tags=[meta_viewport])
+
+    with app.app_context():
+        my_dashapp.title = title
+        my_dashapp.layout = layout
+        if register_callbacks_fun:
+            register_callbacks_fun(my_dashapp)
